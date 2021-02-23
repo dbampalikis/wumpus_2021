2505a175c87353739afd1a1b8b9262c4d56e0ab8
dbampalikis <dimitris.bampalikis@gmail.com>
2021-02-23 09:00:44 +0100

    WIP inital implementation

--- Wumpus_v1-1/Wumpus_World/src/main/java/partialObservability/MyAI.java
+++ Wumpus_v1-1/Wumpus_World/src/main/java/partialObservability/MyAI.java
@@ -1,13 +1,16 @@
 package partialObservability;
+import fullObservability.SearchAI;
 import org.tweetyproject.logics.pl.reasoner.*;
 import org.tweetyproject.logics.pl.sat.*;
 import org.tweetyproject.logics.pl.syntax.*;
 import org.tweetyproject.logics.pl.parser.*;
 import wumpus.Agent;
+import wumpus.World;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MyAI extends Agent
@@ -43,24 +46,66 @@ public class MyAI extends Agent
 
 	}
 
+	public static class State {
+		public int positionX;
+		public int positionY;
+		public boolean gold;
+		public int direction; // The direction the agent is facing: 0 - right, 1 - down, 2 - left, 3 - up
+		public int gscore;
+		public int tscore;
+		public boolean arrow;
+		public boolean wumpus;
+		public int maxRow;
+		public int maxCol;
+
+		public State(int positionX, int positionY, boolean gold_retrieved, int direction, int gscore, int tscore, boolean arrow, boolean wumpus, int maxRow, int maxCol) {
+			this.positionX = positionX;
+			this.positionY = positionY;
+			this.gold = gold_retrieved;
+			this.direction = direction;
+			this.gscore = gscore;
+			this.tscore = tscore;
+			this.arrow = arrow;
+			this.wumpus = wumpus;
+			this.maxRow = maxRow;
+			this.maxCol = maxCol;
+		}
+	}
+
+
+
 	boolean DEBUG = true;
+	// Variables for the size of the board
+	int numCol = -1;
+	int numRow = -1;
+	int maxCol = 1;
+	int maxRow = 1;
 
 	public PlFormula MakeQuestion(String Letter, String XY) {
 		return new Negation(new Proposition(Letter + XY));
 	}
 
-	public boolean Ask(PlBeliefSet bs, PlFormula question) {
+	public boolean Ask(PlBeliefSet bs, String symbol) {
 
 		// Solver setup.
 		AbstractPlReasoner r = new SatReasoner();
 		SatSolver.setDefaultSolver(new Sat4jSolver());
 
-		// True if the second formula is entailed be the first formula.
-		boolean answer = r.query(bs, question);
-		if (DEBUG) System.out.println("Is " + question + " true? " + answer);
+		PlParser plParser = new PlParser();
+		boolean answer = false;
+
+		try {
+			PlFormula f = plParser.parseFormula(symbol);
+			answer = r.query(bs, f);
+			if (DEBUG) System.out.println("Is " + f + " true? " + answer);
+		} catch (IOException e) {
+			System.out.println(e.getStackTrace());
+		}
 
 		return answer;
 	}
+
+	public static ArrayList<String> safeTiles = new ArrayList<String>();
 
 	public Action getAction
 			(
@@ -74,40 +119,125 @@ public class MyAI extends Agent
 
 		try {
 
-			ArrayList<String> safeTiles = new ArrayList<String>();
+
 			ArrayList<String> visitedTiles = new ArrayList<String>();
 			PlBeliefSet bs = new PlBeliefSet();
 			PlParser plParser = new PlParser();
 			PlFormula question;
 			boolean answer;
-			String[] Letters;
-			String[] Coordinates;
+			String[] symbols;
+			ArrayList<String> neighbors;
 			int X, Y;
 
-
+			System.out.println("Safe tiles!!!:" + safeTiles);
 			// Default knowledge ---------------------------------------------------------------------------------------
-			safeTiles.add("11");
-			bs.add((PlFormula) new Negation(new Proposition ("P11")));
+			safeTiles.add("00");
+			bs.add((PlFormula) new Negation(new Proposition ("P00")));
 
 
 			// [1,1] ---------------------------------------------------------------------------------------------------
-			X = 1; Y = 1;
+			X = 0; Y = 0;
 			if (DEBUG) System.out.println("\n[" + X + "," + Y + "]");
 			visitedTiles.add("" + X + Y);
 
+			// Define initial state
+			SearchAI.State currentState;
+			currentState = new SearchAI.State(0, 0, false, 0, 0, Integer.MAX_VALUE, true, true);
 
-			Proposition b = new Proposition("B" + X + Y);
-			if (breeze) {
-				bs.add(b);
-			} else {
-				bs.add((PlFormula) b.complement());
+
+			// Check if the tile contains the gold
+			if(glitter) {
+				if(currentState.gold) {
+					// TODO: Call search AI to find optimal path
+					// TODO: Possibly store this to a global var and pick from that every time
+				} else {
+					return Action.GRAB;
+				}
 			}
+
+			// Make-percept-sentence
+			// Breeze
+			Proposition p = new Proposition("B" + currentState.positionX + currentState.positionY);
+			if (breeze) {
+				bs.add(p);
+			} else {
+				bs.add((PlFormula) p.complement());
+			}
+			bs.add(plParser.parseFormula(createDoubleImplication("B", currentState)));
+			System.out.println("BS:" + bs);
+
+			// Stench
+			p = new Proposition("S" + X + Y);
+			if (stench) {
+				bs.add(p);
+			} else {
+				bs.add((PlFormula) p.complement());
+			}
+			bs.add(plParser.parseFormula(createDoubleImplication("S", currentState)));
+			System.out.println("BS:" + bs);
+
+			// Scream
+			p = new Proposition("C" + X + Y);
+			if (scream) {
+				bs.add(p);
+			} else {
+				bs.add((PlFormula) p.complement());
+			}
+
+			// Bump
+			if (bump) {
+				bs.add(p);
+				// The direction the agent is facing: 0 - right, 1 - down, 2 - left, 3 - up
+				// If there is no bump and the current position is bigger than the size of the map
+				switch(currentState.direction)
+				{
+					case 0:
+						numCol = currentState.positionX;
+						break;
+					case 3:
+						numRow = currentState.positionY;
+						break;
+				}
+			} else {
+
+				bs.add((PlFormula) p.complement());
+			}
+
+
+			// Line safe <- {[x,y]: ASK(KB, OK) = true}
+			// Questions - answers.
+			symbols = new String[]{"P", "W", "!P", "!W"};
+			neighbors = getNeighbors(currentState);
+
+			for (String neighbor : neighbors) {
+				for (String symbol : symbols) {
+
+					answer = Ask(bs, symbol+neighbor);
+					if (answer) {
+						// Add this to the KB.
+						bs.add(plParser.parseFormula(symbol+neighbor));
+					}
+				}
+				answer = Ask(bs, "!P"+neighbor+"&&!W"+neighbor);
+				if (answer) {
+					// Add to safe tiles.
+					safeTiles.add(neighbor);
+					if (DEBUG) System.out.println("Tile " + neighbor + " is safe");
+				}
+			}
+
+
+
+
+
+
+/*
 
 			// Figure out here that we have 2 tiles to inspect: P12, P21.
 			// Then add <=> to the KB.
 			// Then check each tile with questions.
 
-			bs.add(plParser.parseFormula("B11 <=> (P12 || P21)")); // Do be generated automatically.
+			bs.add(plParser.parseFormula("B11 <=> (P12 || P21)")); // To be generated automatically.
 			if (DEBUG) System.out.println("KB in [" + X + "," + Y + "]: " + bs);
 
 			// Questions - answers.
@@ -178,12 +308,14 @@ public class MyAI extends Agent
 			Coordinates = new String[]{"22", "13", "31"};
 
 
-			/*
+			*/
+/*
 			!!!!!!!!!!!! We should figure out here that 31 has a pit !!!!!!!!!!!!
 			 Most probably we just need to ask both questions: Pxy and !Pxy.
 			 Whichever returns `true` should be added to the KB.
 			 It means that the if-statement `if (answer)` needs to be changed.
-			*/
+			*//*
+
 
 			for (String l : Letters) {
 				for (String c : Coordinates) {
@@ -232,6 +364,7 @@ public class MyAI extends Agent
 				}
 			}
 
+*/
 
 
 			if (DEBUG) System.out.println("Visited tiles: " + visitedTiles);
@@ -263,4 +396,64 @@ public class MyAI extends Agent
 
 	}
 
+
+	public String createDoubleImplication(String source, SearchAI.State state) {
+		String implication = "";
+		ArrayList<String> neighbors = getNeighbors(state);
+		String symbol = "";
+		switch(source)
+		{
+			case "B":
+				symbol = "P";
+				break;
+			case "S":
+				symbol = "W";
+				break;
+		}
+
+		implication = source + state.positionX + state.positionY + " <=> (" + symbol + neighbors.get(0);
+
+		for(String neighbor : neighbors.subList(1, neighbors.size())) {
+			implication = implication + " || " + symbol + neighbor;
+		}
+		implication = implication + ")";
+
+
+		System.out.println("Implication: " + implication);
+		return implication;
+	}
+
+	public ArrayList<String> getNeighbors(SearchAI.State state) {
+
+		ArrayList<String> neighbors = new ArrayList<>();
+
+		if(state.positionX > 0) {
+			neighbors.add("" + (state.positionX-1) + state.positionY);
+		}
+		if(state.positionY > 0) {
+			neighbors.add("" + (state.positionX) + (state.positionY-1));
+		}
+		if(numRow == -1 || state.positionX < numRow) {
+			neighbors.add("" + (state.positionX+1) + state.positionY);
+		}
+		if(numCol == -1 || state.positionX < numCol) {
+			neighbors.add("" + (state.positionX) + (state.positionY+1));
+		}
+
+		return neighbors;
+	}
+
+	public World.Tile[][] createBoard() {
+		World.Tile[][] board = new World.Tile[numRow][numCol];
+
+
+		for ( int r = 0; r < rowDimension; ++r ) {
+			for (int c = 0; c < colDimension; ++c) {
+				board[c][r] = new board.Tile();
+
+			}
+		}
+
+		return
+	}
 }
